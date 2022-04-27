acff3993dc5120f1ea5eec06e114be0e0bed6caf
Dylan Nolan <dylannolan@Dylan-Nolans-MacBook-Pro.local>
2022-04-27 18:30:45 +0100

    fixed the data not loading

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -1,5 +1,6 @@
 package ie.tudublin;
 
+import processing.core.PApplet;
 import processing.data.TableRow;
 
 //Nematode Class
@@ -8,67 +9,74 @@ public class Nematode {
     private String name;
     private int length;
     private Boolean limbs;
-    private String gender;
+    private char gender;
     private Boolean eyes;
 
-    //toString
+    // toString
     @Override
     public String toString() {
         return "Nematode [eyes=" + eyes + ", gender=" + gender + ", length=" + length + ", limbs=" + limbs + ", name="
                 + name + "]";
     }
 
-    //TableRow 
-    public Nematode(TableRow tr)
-    {
-        this(   tr.getString("name"),
+    // TableRow
+    public Nematode(TableRow tr) {
+        this(tr.getString("name"),
                 tr.getInt("length"),
                 tr.getInt("limbs") == 1,
-                tr.getString("gender"),
-                tr.getInt("eyes") == 1 
-            );
+                tr.getString("gender").charAt(0),
+                tr.getInt("eyes") == 1);
 
     }
 
-    //Constructors
-    public Nematode(String name, int length, Boolean limbs, String gender, Boolean eyes) {
+    // Constructors
+    public Nematode(String name, int length, Boolean limbs, char gender, Boolean eyes) {
         this.name = name;
         this.length = length;
         this.limbs = limbs;
         this.gender = gender;
         this.eyes = eyes;
     }
-    
-    //Accessors
+
+    // Accessors
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getLength() {
         return length;
     }
+
     public void setLength(int length) {
         this.length = length;
     }
+
     public Boolean getLimbs() {
         return limbs;
     }
+
     public void setLimbs(Boolean limbs) {
         this.limbs = limbs;
     }
-    public String getGender() {
+
+    public char getGender() {
         return gender;
     }
-    public void setGender(String gender) {
+
+    public void setGender(char gender) {
         this.gender = gender;
     }
+
     public Boolean getEyes() {
         return eyes;
     }
+
     public void setEyes(Boolean eyes) {
         this.eyes = eyes;
     }
-    
+
 }
--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -6,52 +6,50 @@ import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
 
-
-public class NematodeVisualiser extends PApplet
-{
-	//Nematode array
+public class NematodeVisualiser extends PApplet {
+	// Nematode array
 	ArrayList<Nematode> nematodes = new ArrayList<Nematode>();
 
-	public void keyPressed()
-	{		
-		if (keyCode == LEFT)
-		{
-		}		
+	public void keyPressed() {
+		if (keyCode == LEFT) {
+		}
 	}
 
-
-	public void settings()
-	{
+	public void settings() {
 		size(800, 800);
-		draw(); 
+
 	}
 
-	public void setup() 
-	{
+	public void setup() {
 		colorMode(HSB);
 		background(0);
-		smooth();				
-	}
-	
+		smooth();
 
-	public void loadNematodes()
-	{
-		//load the data
+		loadNematodes();
+		printData();
+	}
+
+	public void loadNematodes() {
+		// load the data
 		Table table = loadTable("nematodes.csv", "header");
 
-        for(TableRow r:table.rows())
-        {
-            Nematode nematode = new Nematode(r);
-            nematodes.add(nematode);
-        }
+		for (TableRow r : table.rows()) {
+			Nematode nematode = new Nematode(r);
+			nematodes.add(nematode);
+		}
 	}
 
-
-	public void draw()
-	{
-		for(Nematode nematode:nematodes){
+	public void printData() {
+		for (Nematode nematode : nematodes) {
 			System.out.println(nematode);
 		}
 	}
-	
+
+	public void draw() {
+		drawNematodes();
+	}
+
+	public void drawNematodes() {
+	}
+
 }
